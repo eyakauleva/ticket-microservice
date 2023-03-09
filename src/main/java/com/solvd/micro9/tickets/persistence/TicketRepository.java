b7c4a7b780d19faf19a7bb85777ab02e58dc98bf
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-09 14:06:00 +0300

    query fix

--- src/main/java/com/solvd/micro9/tickets/persistence/TicketRepository.java
+++ src/main/java/com/solvd/micro9/tickets/persistence/TicketRepository.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 @Repository
 public interface TicketRepository extends R2dbcRepository<Ticket, Long> {
 
-    @Query(value = "select * from tickets t where t.user_id=:userId")
+    @Query(value = "select t.id, t.user_id, t.event_id, t.quantity, t.price from tickets t where t.user_id=:userId")
     Flux<Ticket> findByUserId(@Param("userId") Long userId);
 
 }
