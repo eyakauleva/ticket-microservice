1a45b2bba1dcaf1d793cf6e1c0f2dea7bb3444f0
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-14 18:57:53 +0300

    kafka impl #10

--- src/main/java/com/solvd/micro9/tickets/domain/exception/ResourceDoesNotExistException.java
+++ src/main/java/com/solvd/micro9/tickets/domain/exception/ResourceDoesNotExistException.java
@@ -1,6 +1,7 @@
 package com.solvd.micro9.tickets.domain.exception;
 
 public class ResourceDoesNotExistException extends RuntimeException {
+
     public ResourceDoesNotExistException(String message) {
         super(message);
     }
