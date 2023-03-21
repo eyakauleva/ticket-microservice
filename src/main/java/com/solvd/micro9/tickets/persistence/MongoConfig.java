96ef790f5a29bd8ca67d50baf12830e04b8dbcb8
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-21 18:42:13 +0300

    try mongo connection fix

--- src/main/java/com/solvd/micro9/tickets/persistence/MongoConfig.java
+++ src/main/java/com/solvd/micro9/tickets/persistence/MongoConfig.java
@@ -1,24 +1,24 @@
-package com.solvd.micro9.tickets.persistence;
-
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-
-@Configuration
-@EnableReactiveMongoRepositories
-public class MongoConfig extends AbstractReactiveMongoConfiguration {
-
-    @Bean
-    public MongoClient mongoClient() {
-        return MongoClients.create();
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return "tickets";
-    }
-
-}
+//package com.solvd.micro9.tickets.persistence;
+//
+//import com.mongodb.reactivestreams.client.MongoClient;
+//import com.mongodb.reactivestreams.client.MongoClients;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+//import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+//
+//@Configuration
+//@EnableReactiveMongoRepositories
+//public class MongoConfig extends AbstractReactiveMongoConfiguration {
+//
+//    @Bean
+//    public MongoClient mongoClient() {
+//        return MongoClients.create();
+//    }
+//
+//    @Override
+//    protected String getDatabaseName() {
+//        return "tickets";
+//    }
+//
+//}
