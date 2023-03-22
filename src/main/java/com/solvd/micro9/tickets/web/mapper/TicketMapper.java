ebc5c4d42380a8ee9a2c0e1d6eb0c99b97819f5c
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-22 14:18:28 +0300

    relation fix

--- src/main/java/com/solvd/micro9/tickets/web/mapper/TicketMapper.java
+++ src/main/java/com/solvd/micro9/tickets/web/mapper/TicketMapper.java
@@ -1,11 +1,8 @@
 package com.solvd.micro9.tickets.web.mapper;
 
 import com.solvd.micro9.tickets.domain.Ticket;
-import com.solvd.micro9.tickets.web.dto.EventDto;
 import com.solvd.micro9.tickets.web.dto.TicketDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -22,12 +19,6 @@ public interface TicketMapper {
         return ticketFlux.map(this::domainToDto);
     }
 
-    @Mapping(source = "event", target = "eventId", qualifiedByName = "getEventId")
     Ticket dtoToDomain(TicketDto ticketDto);
-
-    @Named("getEventId")
-    static Long getEventId(EventDto event) {
-        return event.getId();
-    }
 
 }
