a9bc3736ad7bce151030caecbb64494a628cf299
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-22 14:58:48 +0300

    mongo reference fix

--- src/main/java/com/solvd/micro9/tickets/domain/Ticket.java
+++ src/main/java/com/solvd/micro9/tickets/domain/Ticket.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.math.BigDecimal;
@@ -28,7 +28,7 @@ public class Ticket {
     @Field(name = "user_id")
     private Long userId;
 
-    @DBRef
+    @DocumentReference
     private Event event;
 
     private Integer quantity;
