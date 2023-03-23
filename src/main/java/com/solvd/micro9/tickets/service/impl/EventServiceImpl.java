91132c4da3272313a45d3ba0d474a52f72833153
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-23 13:31:01 +0300

    remove extra

--- src/main/java/com/solvd/micro9/tickets/service/impl/EventServiceImpl.java
+++ src/main/java/com/solvd/micro9/tickets/service/impl/EventServiceImpl.java
@@ -6,14 +6,12 @@ import com.solvd.micro9.tickets.persistence.EventRepository;
 import com.solvd.micro9.tickets.persistence.TicketRepository;
 import com.solvd.micro9.tickets.service.EventService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
@@ -25,7 +23,6 @@ public class EventServiceImpl implements EventService {
 
     public Flux<Event> findByUserId(Long userId) {
         Flux<Ticket> ticketFlux = ticketRepository.findByUserId(userId);
-        ticketFlux.subscribe(ticket -> log.info("ticket: " + ticket));
         return ticketFlux.flatMap(ticket -> eventRepository.findById(ticket.getEventId()))
                 .distinct(Event::getId);
     }
